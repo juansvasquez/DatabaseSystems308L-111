89cd2b9095c17f4eae63ba8a3918babc72912e71
jvasquez1 <juansebvas@gmail.com>
2016-02-08 16:38:42 -0500

    Juan S. Vasquez, Lab 3, 2/8/16

---
+++ Vasquez, Juan Lab 3.sql
@@ -0,0 +1,22 @@
+﻿
+--Juan S. Vasquez
+--2/8/2016
+--Lab 3: Getting Started with SQL Queries
+
+SELECT ordnum, totalUSD FROM orders;
+
+SELECT name, city FROM agents WHERE name = 'Smith';
+
+SELECT pid, name, priceUSD FROM products WHERE quantity > 208000;
+
+SELECT name, city FROM customers WHERE city = 'Dallas';
+
+SELECT name FROM agents WHERE city != 'New York' AND city != 'Tokyo';
+
+SELECT * FROM products WHERE city != 'Dallas' AND city != 'Duluth' AND priceUSD >= 1.00;
+
+SELECT * FROM orders WHERE mon = 'jan' OR mon = 'mar';
+
+SELECT * FROM orders WHERE mon = 'feb' AND totalUSD < 500;
+
+SELECT * FROM orders WHERE cid = 'C005';
