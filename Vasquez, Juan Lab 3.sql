dcd869c809bcbfb8c7ec21ac8abf8b277a6028bd
jvasquez1 <juansebvas@gmail.com>
2016-02-10 21:36:24 -0500

    Juan S. Vasquez, Lab 3, 2/8/16

--- Vasquez, Juan Lab 3.sql
+++ Vasquez, Juan Lab 3.sql
@@ -3,20 +3,55 @@
 --2/8/2016
 --Lab 3: Getting Started with SQL Queries
 
-SELECT ordnum, totalUSD FROM orders;
+--1. List the order number and total dollars of	all orders.
 
-SELECT name, city FROM agents WHERE name = 'Smith';
+SELECT ordnum, totalUSD 
+FROM orders;
 
-SELECT pid, name, priceUSD FROM products WHERE quantity > 208000;
+--2. List the name and city of agents named Smith.
 
-SELECT name, city FROM customers WHERE city = 'Dallas';
+SELECT name, city 
+FROM agents 
+WHERE name = 'Smith';
 
-SELECT name FROM agents WHERE city != 'New York' AND city != 'Tokyo';
+--3. List the id, name, and priceUSD of	products with quantity more than 208,000.
 
-SELECT * FROM products WHERE city != 'Dallas' AND city != 'Duluth' AND priceUSD >= 1.00;
+SELECT pid, name, priceUSD 
+FROM products 
+WHERE quantity > 208000;
 
-SELECT * FROM orders WHERE mon = 'jan' OR mon = 'mar';
+--4. List the names and cities of customers in Dallas.
 
-SELECT * FROM orders WHERE mon = 'feb' AND totalUSD < 500;
+SELECT name, city 
+FROM customers 
+WHERE city = 'Dallas';
 
-SELECT * FROM orders WHERE cid = 'C005';
+--5. List the names of agents not in New York and not in Tokyo.
+
+SELECT name 
+FROM agents 
+WHERE city != 'New York' AND city != 'Tokyo';
+
+--6. List all data for products not in Dallas or Duluth that cost US$1 or more.
+
+SELECT * 
+FROM products 
+WHERE city NOT IN ('Dallas','Duluth') AND priceUSD >= 1.00;
+
+--7. List all data for orders in January or March.
+
+SELECT * 
+FROM orders 
+WHERE mon = 'jan' OR mon = 'mar';
+
+--8. List all data for orders in February less than us$500.
+
+SELECT * 
+FROM orders 
+WHERE mon = 'feb' AND totalUSD < 500;
+
+--9. List all orders from the customer whose cid is C005.
+
+SELECT * 
+FROM orders 
+WHERE cid = 'C005';
